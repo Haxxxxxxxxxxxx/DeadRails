3c7b7aeb8284e1a6ff65981a71fe6632c0907a1d
Haxxxxxxxxxxxx <r4mpage4@gmail.com>
2025-04-06 18:48:15 +0800

    Update HaxWareV2.lua

--- HaxWareV2.lua
+++ HaxWareV2.lua
@@ -1,10 +1,3 @@
-local players = game:GetService("Players")
-local name, version = identifyexecutor()
-
-if name ~= "Solara" and name ~= "Nezur" and name ~= "Xeno" then
-    players.LocalPlayer:Kick("Unsupported Executor")
-end
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Haxxxxxxxxxxxx/DeadRails/refs/heads/main/HaxWareV2.lua"))()
 
 -- https://discord.gg/6MXWqpAj
