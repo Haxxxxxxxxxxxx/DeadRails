e2fb5673143524c215e3af9993f7e882f45f194a
Haxxxxxxxxxxxx <r4mpage4@gmail.com>
2025-04-06 19:33:56 +0800

    Update HaxWareV2.lua

--- HaxWareV2.lua
+++ HaxWareV2.lua
@@ -1,3 +1,22 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Haxxxxxxxxxxxx/nope/refs/heads/main/supported.lua"))();
-
 -- https://discord.gg/6MXWqpAj
+local players = game:GetService("Players")
+
+if not identifyexecutor then
+    getgenv().identifyexecutor = function()
+        return "Solara", "v99"
+    end
+end
+
+local name, version = identifyexecutor()
+
+if name == "Solara" or name == "Nezur" or name == "Xeno" then
+    players.LocalPlayer:Kick("Unsupported Executor: " .. name)
+    return
+end
+
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Haxxxxxxxxxxxx/nope/refs/heads/main/supported.lua"))()
+end)
+
+if not success then
+end
